b8c5d742c9b2652947101d9e5787c0fce6d42f21
dionchoy <dionchoy.23@ichat.sp.edu.sg>
2024-05-06 11:27:46 +0800

    Initial raspberry pi release

---
+++ src/PiLedTest.py
@@ -0,0 +1,28 @@
+import hal.hal_input_switch as input_switch
+import hal.hal_led as led
+import time
+
+def blink_led(delay):
+    # Led Blink
+    led.init()
+
+    led.set_output(0, 1)
+    time.sleep(delay)
+
+    led.set_output(0, 0)
+    time.sleep(delay)
+
+
+def main():
+    input_switch.init()
+    led.init()
+
+    while(True):
+        switch = input_switch.read_slide_switch()
+        if switch == 0:
+            blink_led(0.1)
+        elif switch == 1:
+            blink_led(0.2)
+
+if __name__ =='__main__':
+    main()
